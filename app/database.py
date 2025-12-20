e9686c3662bee87ad95846f78eb6d27a9af7c460
Ali Raheel <aliraheel626@gmail.com>
2025-12-20 09:22:34 +0500

    first commit

---
+++ app/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+engine = create_engine("sqlite:///./test.db")
+
+#dependency
+def get_db():
+    with Session(bind=engine, autocommit=False) as session:
+        yield session
